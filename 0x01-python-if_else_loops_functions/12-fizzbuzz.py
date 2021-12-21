93c24b6d55bb6f7482239647a017258140eacd83
MrOmnes <3644@holbertonschool.com>
2021-12-21 21:57:30 +0000

    add 12

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for c in range(1, 101):
+        if c % 3 == 0 and c % 5 != 0:
+            print("Fizz ", end="")
+        if c % 5 == 0 and c % 3 != 0:
+            print("Buzz ", end="")
+        if c % 5 == 0 and c % 3 == 0:
+            print("FizzBuzz", end="")
+        elif c % 5 != 0 and c % 3 != 0:
+            print("{}".format(int(c)), end=" ")
