0fa94a063e3bbc3fc368f5e96be226300971ca6b
MrOmnes <3644@holbertonschool.com>
2021-12-23 15:35:36 +0000

    try another fix

--- 0x02-python-import_modules/100-my_calculator.py
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
-import sys
-from calculator_1 import add, sub, mul, div
 if __name__ == '__main__':
-    if len(sys.argv) != 4:
-        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
-        exit(1)
-    else:
-        a = int(sys.argv[1])
-        b = int(sys.argv[3])
-        if sys.argv[2] == '+':
-            print("{} {} {} = {}".format(str(a), "+", str(b), add(a, b)))
-            exit(0)
-        elif sys.argv[2] == '-':
-            print("{} {} {} = {}".format(str(a), "-", str(b), sub(a, b)))
-            exit(0)
-        elif sys.argv[2] == '*':
-            print("{} {} {} = {}".format(str(a), "*", str(b), mul(a, b)))
-            exit(0)
-        elif sys.argv[2] == '/':
-            print("{} {} {} = {}".format(str(a), "/", str(b), div(a, b)))
-            exit(0)
-        else:
-            print("Unknown operator. Available operators: +, -, * and /")
+    import sys
+    from calculator_1 import add, sub, mul, div
+        if len(sys.argv) != 4:
+            print("Usage: ./100-my_calculator.py <a> <operator> <b>")
             exit(1)
+        else:
+            a = int(sys.argv[1])
+            b = int(sys.argv[3])
+            if sys.argv[2] == '+':
+                print("{} {} {} = {}".format(str(a), "+", str(b), add(a, b)))
+                exit(0)
+            if sys.argv[2] == '-':
+                print("{} {} {} = {}".format(str(a), "-", str(b), sub(a, b)))
+                exit(0)
+            if sys.argv[2] == '*':
+                print("{} {} {} = {}".format(str(a), "*", str(b), mul(a, b)))
+                exit(0)
+            if sys.argv[2] == '/':
+                print("{} {} {} = {}".format(str(a), "/", str(b), div(a, b)))
+                exit(0)
+            else:
+                print("Unknown operator. Available operators: +, -, * and /")
+                exit(1)
