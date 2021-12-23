412e7fbb357d3f27addc231091ebb6e09400d471
MrOmnes <3644@holbertonschool.com>
2021-12-23 13:27:14 +0000

    Fix 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -2,14 +2,15 @@
 import sys
 c = len(sys.argv) - 1
 d = 1
-if len(sys.argv) - 1 == 0:
-    print(len(sys.argv) - 1, "arguments.")
-else:
-    if len(sys.argv) - 1 == 1:
-        print(len(sys.argv) - 1, "argument:")
+if __name__ == '__main__':
+    if len(sys.argv) - 1 == 0:
+        print(len(sys.argv) - 1, "arguments.")
     else:
-        print(len(sys.argv) - 1, "arguments:")
-    while(c != 0):
-        print("{}".format(str(d)) + ":", sys.argv[d])
-        d += 1
-        c -= 1
+        if len(sys.argv) - 1 == 1:
+            print(len(sys.argv) - 1, "argument:")
+        else:
+            print(len(sys.argv) - 1, "arguments:")
+        while(c != 0):
+            print("{}".format(str(d)) + ":", sys.argv[d])
+            d += 1
+            c -= 1
