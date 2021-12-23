0106b15d7e3f73c692d25e245748add145db01b8
MrOmnes <3644@holbertonschool.com>
2021-12-23 14:15:10 +0000

    add 4

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import hidden_4
+a = dir(hidden_4)
+i = 1
+c = len(dir(hidden_4))
+while i != 12:
+    if a[i][1] != '_':
+        print("{}".format(str(a[i])))
+        i += 1
+    else:
+        i += 1
