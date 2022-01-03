95f224ddf9db70ae7dba5211e80874a182eb052b
MrOmnes <3644@holbertonschool.com>
2022-01-03 17:25:24 +0000

    add 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,9 +1,4 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    a = -1
-    while matrix[a:]:
-        a += 1
-        b = 0
-        while matrix[a:b]:
-            print(matrix[a:b])
-            b += 1
+    for row in matrix:
+        print(" ".join("{:d}".format(item) for item in row))
