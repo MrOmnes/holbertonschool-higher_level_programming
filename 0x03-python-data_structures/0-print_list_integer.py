8beb2652cad8b3a4396787fbf8f33db26fa4e151
MrOmnes <3644@holbertonschool.com>
2022-01-03 14:09:38 +0000

    Try a fix

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,6 +3,6 @@ def print_list_integer(my_list=[]):
     a = len(my_list)
     b = 0
     while a != 0:
-        print("{:n}".format(int(my_list[b])))
+        print("{:d}".format(int(my_list[b])))
         a -= 1
         b += 1
