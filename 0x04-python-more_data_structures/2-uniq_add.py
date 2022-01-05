d247d1ba9c831767de712db572227d1fe0507f52
MrOmnes <3644@holbertonschool.com>
2022-01-05 16:27:52 +0000

    .

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -4,7 +4,7 @@ def uniq_add(my_list=[]):
     c = 0
     d = 0
     checker = []
-    while i!= 0:
+    while i != 0:
         if my_list[c] not in checker:
             checker.append(my_list[c])
             d += my_list[c]
