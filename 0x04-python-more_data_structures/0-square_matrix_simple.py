88883087a590cf2adb18858d0a07414e9213a787
MrOmnes <3644@holbertonschool.com>
2022-01-05 16:27:04 +0000

    add 2

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -4,7 +4,7 @@ def square_matrix_simple(matrix=[]):
     i = len(matrix)
     c = 0
     while i != 0:
-        new_matrix[c] = pow(new_matrix[c], 2)
+        new_matrix[c] = new_matrix[c] * new_matrix[c]
         i -= 0
         c += 1
     return
---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def uniq_add(my_list=[]):
+    i = len(my_list)
+    c = 0
+    d = 0
+    checker = []
+    while i!= 0:
+        if my_list[c] not in checker:
+            checker.append(my_list[c])
+            d += my_list[c]
+        i -= 1
+        c += 1
+    return d
