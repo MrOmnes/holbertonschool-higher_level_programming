bf9942f2489c9377b0a58e42cfbef03d212ff98d
MrOmnes <3644@holbertonschool.com>
2022-01-17 12:24:12 +0000

    add 2

---
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    value = 0
+    i = x
+    count = 0
+    c = 0
+    for size in my_list:
+        c += 1
+    while i != 0:
+        try:
+            print("{:d}".format(my_list[value]), end="")
+            i -= 1
+            value += 1
+            count += 1
+        except TypeError:
+            i -= 1
+            value +=1
+        except ValueError:
+            i -= 1
+            value +=1
+    print()
+    return count
