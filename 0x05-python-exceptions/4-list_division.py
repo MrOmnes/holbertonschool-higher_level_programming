53b0ca4c492d417501160c0b187c7a9e657b2bb7
MrOmnes <3644@holbertonschool.com>
2022-01-17 13:35:10 +0000

    Fix the 4 for the broken checker

--- 0x05-python-exceptions/4-list_division.py
+++ 0x05-python-exceptions/4-list_division.py
@@ -6,21 +6,16 @@ def list_division(my_list_1, my_list_2, list_length):
     while c != 0:
         try:
             result += [my_list_1[i] / my_list_2[i]]
-            c -= 1
-            i += 1
         except ZeroDivisionError:
             result += [0]
             print("division by zero")
-            c -= 1
-            i += 1
         except TypeError:
             result += [0]
             print("wrong type")
-            c -= 1
-            i += 1
         except IndexError:
             result += [0]
             print("out of range")
         finally:
-            return result
+            c -= 1
+            i += 1
     return result
