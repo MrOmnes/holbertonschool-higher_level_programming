1e51dd7d77869fd0b45d7d67b5b5947626920408
MrOmnes <3644@holbertonschool.com>
2022-01-17 14:51:18 +0000

    fix pycode

--- 0x05-python-exceptions/101-safe_function.py
+++ 0x05-python-exceptions/101-safe_function.py
@@ -5,7 +5,7 @@ import sys
 def safe_function(fct, *args):
     try:
         result = fct(*args)
-    except Exception as e: 
+    except Exception as e:
         result = None
         print("Exception:", e, file=sys.stderr)
     finally:
