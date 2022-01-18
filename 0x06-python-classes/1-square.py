1a99f8850d3925664ffb278cc181072de8d1a2c1
MrOmnes <3644@holbertonschool.com>
2022-01-18 14:48:48 +0000

    add 1

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,10 @@
 #!/usr/bin/python3
+"""1-square.py:
+    This is just a Square
+"""
+
+
 class Square:
+    """Definie the size of the square"""
     def __init__(self, size):
         self.__size = 3
