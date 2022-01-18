c09534f637dc4d5d2b6700024b7ad2a3f148b9ba
MrOmnes <3644@holbertonschool.com>
2022-01-18 22:36:36 +0000

    add comment

--- 0x06-python-classes/103-magic_class.py
+++ 0x06-python-classes/103-magic_class.py
@@ -1,8 +1,26 @@
 #!/usr/bin/python3
+"""3-square.py:
+    This is just a Square
+"""
+
 import math
 
 class MagicClass:
+    """
+    Definie the size of the square
+
+    Attributes
+    ----------
+    size : int
+        The size of the square.
+    """
     def __init__(self, radius=0):
+        """
+        Parameters
+        ----------
+        radius : int
+            The size of the square.
+        """
         self.__radius = 0
         if type(radius) is not int and type(radius) is not float:
             raise TypeError("radius must be a number")
