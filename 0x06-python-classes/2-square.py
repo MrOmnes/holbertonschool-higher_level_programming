4ae3227ba76ea2ce281d1c827e36d42214c4ba20
MrOmnes <3644@holbertonschool.com>
2022-01-18 15:08:40 +0000

    add 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,9 +1,28 @@
 #!/usr/bin/python3
+"""2-square.py:
+    This is just a Square
+"""
+
+
 class Square:
+    """
+    Definie the size of the square
+
+    Attributes
+    ----------
+    size : int
+        The size of the square.
+    """
     def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size : int
+            The size of the square.
+        """
         if type(size) != int:
             raise TypeError("size must be an integer")
         if size < 0:
             raise ValueError("size must be >= 0")
-        elif type(size) == int and size >=0:
+        elif type(size) == int and size >= 0:
             self.__size = size
