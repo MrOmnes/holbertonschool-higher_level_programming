b964152ceaaa12a3c183b672a569c02bdc57f92c
MrOmnes <3644@holbertonschool.com>
2022-01-18 16:22:19 +0000

    fix pycode

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -26,5 +26,6 @@ class Square:
             raise ValueError("size must be >= 0")
         elif type(size) == int and size >= 0:
             self.__size = size
+
     def area(self):
         return self.__size * self.__size
