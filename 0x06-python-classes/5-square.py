62acc91bab6e24378ae32b2632b9b76c464ab873
MrOmnes <3644@holbertonschool.com>
2022-01-18 18:44:29 +0000

    add 5

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,57 @@
+#!/usr/bin/python3
+"""3-square.py:
+    This is just a Square
+"""
+
+
+class Square:
+    """
+    Definie the size of the square
+
+    Attributes
+    ----------
+    size : int
+        The size of the square.
+    """
+    def __init__(self, size=0):
+        """
+        Parameters
+        ----------
+        size : int
+            The size of the square.
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        elif type(size) == int and size >= 0:
+            self.__size = size
+
+    @property
+    def size(self):
+        if type(self.__size) != int:
+            raise TypeError("size must be an integer")
+        elif type(self.__size) == int:
+            return self.__size
+
+    @size.setter
+    def size(self, value):
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif type(value) == int:
+            self.__size = value
+
+    def area(self):
+        return self.__size * self.__size
+
+    def my_print(self):
+        c = self.__size
+        i = self.__size
+        while c != 0:
+            print('#', end="")
+            c -= 1
+            if i != 1 and c == 0:
+                print("")
+                i -= 1
+                c = self.__size
+        print("")
