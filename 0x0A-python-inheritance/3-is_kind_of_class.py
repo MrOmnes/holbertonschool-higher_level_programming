b015a44b232ba47f2aa1cd972dbf73e3988ff617
MrOmnes <3644@holbertonschool.com>
2022-01-31 17:15:48 +0000

    Fix 3 comments

--- 0x0A-python-inheritance/3-is_kind_of_class.py
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -1,6 +1,14 @@
 #!/usr/bin/python3
+"""3-is_kind_of_class.py:
+    Check if obj is kind of a_class
+"""
+
+
 def is_kind_of_class(obj, a_class):
-	if isinstance(obj, a_class):
-		return True
-	else:
-		return False
+    """
+    Check if obj is kind of a_class
+    """
+    if isinstance(obj, a_class):
+        return True
+    else:
+        return False
