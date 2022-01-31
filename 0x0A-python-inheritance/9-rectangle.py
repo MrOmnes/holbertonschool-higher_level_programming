12e15b19e272a2065a9f79a7297a3c085724e38f
MrOmnes <3644@holbertonschool.com>
2022-01-31 17:48:12 +0000

    fix 9 comments

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,22 +1,21 @@
 #!/usr/bin/python3
-class BaseGeometry:
-	def area(self):
-		raise Exception("area() is not implemented")
+"""9-rectangle.py:
+    Set size and area of a rectangle
+"""
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
-	def integer_validator(self, name, value):
-		if type(value) is not int:
-			raise TypeError(name + " must be an integer")
-		if value <= 0:
-			raise ValueError(name + " must be greater than 0")
-		return value
 
 class Rectangle(BaseGeometry):
-	def __init__(self, width, height):
-		self.__width = BaseGeometry.integer_validator(self, "width", width)
-		self.__height = BaseGeometry.integer_validator(self, "height", height)
+    """Create size and area of a rectangle"""
+    def __init__(self, width, height):
+        """Set width and height"""
+        self.__width = BaseGeometry.integer_validator(self, "width", width)
+        self.__height = BaseGeometry.integer_validator(self, "height", height)
 
-	def area(self):
-		return self.__width * self.__height
+    def area(self):
+        """Set area"""
+        return self.__width * self.__height
 
-	def __str__(self):
-		return ("[Rectangle] {}/{}".format(self.__width, self.__height))
+    def __str__(self):
+        """Return"""
+        return ("[Rectangle] {}/{}".format(self.__width, self.__height))
