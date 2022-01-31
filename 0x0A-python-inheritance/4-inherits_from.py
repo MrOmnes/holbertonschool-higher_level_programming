9784bf376d72605f3c8b10fe8032a0a8a5a4582c
MrOmnes <3644@holbertonschool.com>
2022-01-31 19:34:12 +0000

    Fix 4

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
+"""Check if is inherits from"""
+
+
 def inherits_from(obj, a_class):
-	if isinstance(type(obj), a_class):
-		return True
-	else:
-		return False
+    """Check if is inherits from"""
+    return issubclass(type(obj), a_class) and type(obj) != a_class
