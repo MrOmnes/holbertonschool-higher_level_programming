efbac46a2110146e078e865290c5c8276e0743fd
MrOmnes <3644@holbertonschool.com>
2022-01-31 20:03:16 +0000

    Add 13

--- 0x0A-python-inheritance/101-add_attribute.py
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -4,3 +4,9 @@
 """
 
 
+def add_attribute(obj, name, value):
+    """Add an attribute if is not already in the class"""
+    if hasattr(obj, "__dict__") is False:
+        raise TypeError("can't add new attribute")
+    else:
+        setattr(obj, name, value)
