49ff2b46c5e17ff6f28d0b00965da6ca3ed50324
MrOmnes <3644@holbertonschool.com>
2022-02-01 16:00:07 +0000

    Try a fix on the 0

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """Read a file"""
+
+
 def read_file(filename=""):
     """Read a file"""
-    with open(filename) as f:
+    with open(filename, "r") as f:
         print(f.read())
