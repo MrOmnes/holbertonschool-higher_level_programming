692ef505f48a4d7659e39129b4643ee2d45bc296
MrOmnes <3644@holbertonschool.com>
2022-02-01 15:51:47 +0000

    Fix comment on 2

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+"""Append on a file"""
+
+
 def append_write(filename="", text=""):
+    """Append on a file"""
     with open(filename, "a") as f:
         f.write(text)
     return len(text)
