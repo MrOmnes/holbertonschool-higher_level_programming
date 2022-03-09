ab93de177f6558dce5bea1707b7f0656c1d76211
MrOmnes <3644@holbertonschool.com>
2022-03-09 16:31:22 +0000

    fix 9 order

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- List Cities
-SELECT * FROM cities JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC
+SELECT cities.name, cities.id, states.id FROM cities JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC
