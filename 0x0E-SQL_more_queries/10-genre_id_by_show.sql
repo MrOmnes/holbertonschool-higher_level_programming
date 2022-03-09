9bfb97927569ab5d0c74a164ef2de67523da0bde
MrOmnes <3644@holbertonschool.com>
2022-03-09 15:51:52 +0000

    Try a blinded fix

--- 0x0E-SQL_more_queries/10-genre_id_by_show.sql
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -1,2 +1,3 @@
 -- Netflix 
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres WHERE tv_show_genres.genre_id IS NOT NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
+UPDATE tv_show_genres SET genre_id = 'NULL' WHERE genre_id IS NULL;
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
