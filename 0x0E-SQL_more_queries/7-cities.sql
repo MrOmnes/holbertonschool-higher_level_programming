a46099673ac33ab5daecec63021ef6041ec36d5d
MrOmnes <3644@holbertonschool.com>
2022-03-09 13:26:20 +0000

    Add 7

---
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -0,0 +1,8 @@
+-- States
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS cities (
+    id int UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY,
+	state_id int NOT NULL FOREIGN KEY REFERENCES states.id
+    name varchar(256) NOT NULL
+)
