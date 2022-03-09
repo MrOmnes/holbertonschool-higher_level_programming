148fcb90428f81eee5bd3a86931f0326b292b5e4
MrOmnes <3644@holbertonschool.com>
2022-03-09 15:32:36 +0000

    Add 10 but it's weird

---
+++ 0x0E-SQL_more_queries/10-genre_id_by_show.sql
@@ -0,0 +1,2 @@
+-- Netflix 
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows, tv_show_genres WHERE tv_show_genres.genre_id IS NOT NULL ORDER BY tv_shows.title ASC, tv_show_genres.genre_id ASC;
