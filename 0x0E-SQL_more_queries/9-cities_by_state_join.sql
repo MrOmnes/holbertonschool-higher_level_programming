5f25ef9afab69835c102e5c06b1ce6bdb36aef6f
MrOmnes <3644@holbertonschool.com>
2022-03-09 16:31:47 +0000

    I'm dumb

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- List Cities
-SELECT cities.name, cities.id, states.id FROM cities JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC
+SELECT cities.id, cities.name, states.name FROM cities JOIN states ON states.id = cities.state_id ORDER BY cities.id ASC
