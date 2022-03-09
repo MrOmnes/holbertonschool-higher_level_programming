e479ad4404bb38e164db2908cb94b461a1d86b72
MrOmnes <3644@holbertonschool.com>
2022-03-09 13:23:22 +0000

    fix 6

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -2,6 +2,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states (
-    id int UNIQUE PRIMARY KEY,
+    id int UNIQUE NOT NULL AUTO_INCREMENT,
     name varchar(256)
 )
