41f47a1f36e2e1e525030d84e3f21b0a68f8ce78
MrOmnes <3644@holbertonschool.com>
2022-03-09 19:27:28 +0000

    add 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -1,2 +1,2 @@
 -- Netflix
-SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_show_genres RIGHT JOIN tv_shows ON tv_shows.id = tv_show_genres.genre_id WHERE tv_show_genres.genre_id IS NULL
+SELECT tv_shows.title, tv_show_genres.genre_id FROM tv_shows LEFT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id WHERE tv_show_genres.genre_id IS NULL;
