4013229e27d0899bda099b00edbf4904b8adc980
MrOmnes <3644@holbertonschool.com>
2022-04-04 12:55:21 +0000

    Add a fix

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,10 +3,11 @@ let i = 2;
 const values = [];
 if (process.argv[2] === undefined || process.argv[3] === undefined) {
   console.log(0);
-}
-while (process.argv[i] !== undefined) {
-  values.push(process.argv[i]);
-  i++;
-}
+} else {
+  while (process.argv[i] !== undefined) {
+    values.push(process.argv[i]);
+    i++;
+  }
 
-console.log(values.sort().reverse()[1]);
+  console.log(values.sort().reverse()[1]);
+}
