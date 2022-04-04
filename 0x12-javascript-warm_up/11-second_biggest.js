420b7cb9f9d17afd4db88980b737332535262a1f
MrOmnes <3644@holbertonschool.com>
2022-04-04 11:48:47 +0000

    fix 11 semistandard

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,12 +1,12 @@
 #!/usr/bin/node
 let i = 2;
-let values = [];
-if (process.argv[2] === undefined || process.argv[3] === undefined){
-	console.log(0);
+const values = [];
+if (process.argv[2] === undefined || process.argv[3] === undefined) {
+  console.log(0);
 }
 while (process.argv[i] !== undefined) {
-	values.push(process.argv[i]);
-	i++;
+  values.push(process.argv[i]);
+  i++;
 }
 
 console.log(values.sort().reverse()[1]);
