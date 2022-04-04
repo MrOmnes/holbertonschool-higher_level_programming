106fe8de1f2e8ee755d68e452ff8b71b9de9c417
MrOmnes <3644@holbertonschool.com>
2022-04-04 10:45:32 +0000

    Add 10

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const i = parseInt(process.argv[2]);
+
+function factorial (n) {
+  if (Number.isInteger(i) !== 1) {
+    return 1;
+  } else if (n === 0 || n === 1) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+console.log(factorial(i));
