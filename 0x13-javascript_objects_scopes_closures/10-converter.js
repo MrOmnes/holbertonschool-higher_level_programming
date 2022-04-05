c731647102155c9f8c4a4ca82c2cd095177997e9
MrOmnes <3644@holbertonschool.com>
2022-04-05 12:31:46 +0000

    Add 10

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,4 +1,6 @@
 #!/usr/bin/node
-exports.converter = function (base){
-
-}
+exports.converter = function (base) {
+  return function (myConverter) {
+    return myConverter.toString(base);
+  };
+};
