1e14139edd26e1562c234ec7f8cab31bc4bb1188
MrOmnes <3644@holbertonschool.com>
2022-04-05 11:40:45 +0000

    Remove old code

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -2,11 +2,9 @@
 exports.esrever = function (list) {
   const reversed = [];
   let a = list.length - 1;
-  let i = 0;
   while (list[a] !== undefined) {
     reversed.push(list[a]);
     a--;
-    i++;
   }
   return reversed;
 };
