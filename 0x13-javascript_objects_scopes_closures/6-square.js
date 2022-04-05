cd376d581aedea714a019dca5d47337c2e066794
MrOmnes <3644@holbertonschool.com>
2022-04-05 10:31:59 +0000

    Fix 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -1,50 +1,49 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor (w, h) {
-	  if (w > 0 && h > 0) {
-		this.width = w;
-		this.height = h;
-	  }
-	}
-
-	print () {
-	  const myVar = 'X';
-	  let i = this.height;
-	  while (i !== 0) {
-		console.log(myVar.repeat(this.width));
-		i--;
-	  }
-	}
-
-	rotate(){
-		let temp = this.height;
-		this.height = this.width;
-		this.width = temp;
-	}
-
-	double(){
-		this.height = (this.height) * 2;
-		this.width = (this.width) * 2;
-	}
-
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
   }
 
-class Square extends Rectangle {
-	constructor(size){
-		super(size, size);
-	}
+  print () {
+    const myVar = 'X';
+    let i = this.height;
+    while (i !== 0) {
+      console.log(myVar.repeat(this.width));
+      i--;
+    }
+  }
 
-	charPrint(c) {
-		let myVar = c;
-		if (myVar == undefined) {
-			myVar = 'X';
-		}
-		let i = this.height;
-		while (i !== 0) {
-		  console.log(myVar.repeat(this.width));
-		  i--;
-		}
-	}
+  rotate () {
+    const temp = this.height;
+    this.height = this.width;
+    this.width = temp;
+  }
+
+  double () {
+    this.height = (this.height) * 2;
+    this.width = (this.width) * 2;
+  }
+}
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    let myVar = c;
+    if (myVar === undefined) {
+      myVar = 'X';
+    }
+    let i = this.height;
+    while (i !== 0) {
+      console.log(myVar.repeat(this.width));
+      i--;
+    }
+  }
 }
 
 module.exports = Rectangle;
