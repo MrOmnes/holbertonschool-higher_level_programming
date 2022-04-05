577ba79c11d873477c4ec42ea3ed30abc1cb73a7
MrOmnes <3644@holbertonschool.com>
2022-04-05 10:36:18 +0000

    Add 7

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,12 @@
+exports.nbOccurences = function (list, searchElement) {
+  let i = 0;
+  let count = 0;
+  while (list[i] !== undefined) {
+    if (list[i] === searchElement) {
+      count++;
+    }
+    i++;
+  }
+
+  return (count);
+};
