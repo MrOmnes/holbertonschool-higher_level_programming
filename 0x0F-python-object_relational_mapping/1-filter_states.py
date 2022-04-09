f72e3cfa71e389958ebb0e8ec6b08ac7205a2ced
MrOmnes <3644@holbertonschool.com>
2022-04-09 15:53:16 +0000

    Fix pycode on 1

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+"""SELECT where name is starting by N"""
 
 import MySQLdb
 import sys
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=db)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
-result = cursor.fetchall()
+    db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=db)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%'")
+    result = cursor.fetchall()
 
-for x in result:
-    print(x)
+    for x in result:
+        print(x)
