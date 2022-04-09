46f27a5588b42911895bb9a5fa886ec48ae328a7
MrOmnes <3644@holbertonschool.com>
2022-04-09 18:38:05 +0000

    Fix 4

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,17 +1,19 @@
 #!/usr/bin/python3
+"""Cities by state"""
 
 import MySQLdb
 import sys
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=db)
-cursor = db.cursor()
-cursor.execute("SELECT state_id, cities.name, states.name FROM states JOIN\
-    cities WHERE states.id = state_id")
-result = cursor.fetchall()
+    db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=db)
+    cursor = db.cursor()
+    cursor.execute("SELECT state_id, cities.name, states.name FROM states JOIN\
+        cities WHERE states.id = state_id")
+    result = cursor.fetchall()
 
-for x in result:
-    print(x)
+    for x in result:
+        print(x)
