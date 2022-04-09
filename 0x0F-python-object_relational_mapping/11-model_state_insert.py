45d1d7a07826855e76f3f5fcd0d914aafec50313
MrOmnes <3644@holbertonschool.com>
2022-04-09 18:10:32 +0000

    Fix 2

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""Print first states"""
+
+import sys
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    s = 'mysql+mysqldb://'
+    n = sys.argv[1]
+    p = sys.argv[2]
+    b = sys.argv[3]
+    i = '@localhost'
+    engine = create_engine('{}{}:{}{}/{}'.format(s, n, p, i, b))
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = session.execute("INSERT INTO states (name) VALUES ('Louisiana')")
--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
+"""Select states where name =="""
 
 import MySQLdb
 import sys
 
-user = sys.argv[1]
-passwd = sys.argv[2]
-db = sys.argv[3]
+if __name__ == "__main__":
+    user = sys.argv[1]
+    passwd = sys.argv[2]
+    db = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=db)
-cursor = db.cursor()
-cursor.execute("SELECT * FROM states WHERE name='"+ sys.argv[4] + "'")
-result = cursor.fetchall()
+    db = MySQLdb.connect(host='localhost', user=user, passwd=passwd, db=db)
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states WHERE name ='" + sys.argv[4] + "'")
+    result = cursor.fetchall()
 
-for x in result:
-    print(x)
+    for x in result:
+        print(x)
