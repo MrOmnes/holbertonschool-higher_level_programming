8b6e6aa1a3f17ac88b6246364989bf9a913641c4
MrOmnes <3644@holbertonschool.com>
2022-04-10 11:22:28 +0000

    Add 13

---
+++ 0x0F-python-object_relational_mapping/13-model_state_delete_a.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+"""Print all state from states where contain letter a"""
+
+from ast import keyword
+import sys
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from model_state import Base, State
+from sqlalchemy import (create_engine)
+
+if __name__ == "__main__":
+    s = 'mysql+mysqldb://'
+    n = sys.argv[1]
+    p = sys.argv[2]
+    b = sys.argv[3]
+    i = '@localhost'
+    engine = create_engine('{}{}:{}{}/{}'.format(s, n, p, i, b))
+    Base.metadata.create_all(engine)
+
+    from sqlalchemy.orm import sessionmaker
+    Session = sessionmaker(bind=engine)
+    session = Session()
+    result = State.__table__.delete().\
+        where(State.name.op('regexp')('a'.format(keyword)))
+    session.execute(result)
+    session.commit()
