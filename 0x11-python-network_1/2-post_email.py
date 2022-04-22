397c99bfdf76c69f6a8c2de9ab3f8b88142918ce
Omnes <kevin@streamrunners.fr>
2022-04-22 19:44:16 +0000

    Try a little fix

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,11 +5,9 @@ import urllib.request
 import urllib.parse
 import sys
 
-url = sys.argv[1]
-params = {"email": sys.argv[2]}
-
-
 if __name__ == "__main__":
+    url = sys.argv[1]
+    params = {"email": sys.argv[2]}
     query_string = urllib.parse.urlencode(params)
     url = url + "?" + query_string
     with urllib.request.urlopen(url) as response:
