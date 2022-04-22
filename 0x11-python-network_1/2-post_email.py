6fc56e76e20e92096e671d61d5e7a0a5ed105fa5
Omnes <kevin@streamrunners.fr>
2022-04-22 18:28:53 +0000

    Trust on my code

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -5,7 +5,7 @@ from urllib import request, parse
 import sys
 
 if __name__ == "__main__":
-	data = parse.urlencode({"email":"test"}).encode()
+	data = parse.urlencode({"email": sys.argv[2]}).encode()
 	req = request.Request(sys.argv[1], data=data)
 	resp = request.urlopen(req)
 	print(req)
