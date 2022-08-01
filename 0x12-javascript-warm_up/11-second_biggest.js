11833569d368375d1fa5a3cdd080bd155c1e7576
Omnes <kevin@streamrunners.fr>
2022-08-01 14:13:57 +0000

    try something

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -3,11 +3,9 @@
 process.argv.shift();
 process.argv.shift();
 
-const array = process.argv.sort();
+const array = process.argv.sort().reverse();
 if (array.length < 2) {
   console.log(0);
 } else {
-  array.reverse();
-
   console.log(array[1]);
 }
