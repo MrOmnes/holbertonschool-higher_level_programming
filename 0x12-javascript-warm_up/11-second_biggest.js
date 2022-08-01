395fe1250e538ea356867cfdb53a9ece91f89015
Omnes <kevin@streamrunners.fr>
2022-08-01 11:11:12 +0000

    rework

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,15 +2,12 @@
 
 process.argv.shift();
 process.argv.shift();
-const array = process.argv.sort();
-array.reverse();
 
-if (!process.argv[1]) {
+const array = process.argv.sort();
+if (array.length < 2) {
   console.log(0);
 } else {
-  if (!process.argv[2]) {
-    console.log(array[0]);
-  } else {
-    console.log(array[1]);
-  }
+  array.reverse();
+
+  console.log(array[1]);
 }
