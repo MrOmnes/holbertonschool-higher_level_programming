298e228401d887f0e27eb487ab8eaaebe948b096
Omnes <kevin@streamrunners.fr>
2022-08-01 11:18:25 +0000

    add a try

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,6 +2,9 @@
 
 process.argv.shift();
 process.argv.shift();
+for (let i = 0; i < process.argv.length; i++) {
+    process.argv[i] = parseInt(process.argv[i]);
+}
 
 const array = process.argv.sort();
 if (array.length < 2) {
