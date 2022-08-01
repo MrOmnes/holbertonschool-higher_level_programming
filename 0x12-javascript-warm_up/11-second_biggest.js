720ada4740094b3f21ddc885933c04e73628bf6b
Omnes <kevin@streamrunners.fr>
2022-08-01 10:52:26 +0000

    fix an issu where there are less than 3 numbers

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -8,5 +8,9 @@ array.reverse();
 if (!process.argv[1]) {
   console.log(0);
 } else {
-  console.log(process.argv[1]);
+  if (!process.argv[2]) {
+    console.log(process.argv[0]);
+  } else {
+    console.log(process.argv[1]);
+  }
 }
