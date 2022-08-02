dc9aca8dede2e41d0eb501467835d1be3d1c2a2e
Omnes <kevin@streamrunners.fr>
2022-08-02 10:20:52 +0000

    add 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+const Rectangle = class {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    let i = this.height;
+    while (i !== 0) {
+      console.log('X'.repeat(this.width));
+      i--;
+    }
+  }
+
+  rotate () {
+    this.temp = this.height;
+    this.height = this.width;
+    this.width = this.temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
+
+module.exports = Rectangle;
