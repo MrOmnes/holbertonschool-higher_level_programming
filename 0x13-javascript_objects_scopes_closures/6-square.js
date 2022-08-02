7080f7e4a3b765647125f855ae5ec17a68e5be0f
Omnes <kevin@streamrunners.fr>
2022-08-02 11:00:33 +0000

    add 6

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,47 @@
+#!/usr/bin/node
+const Rectangle = class {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+      this.carac = 'X';
+    }
+  }
+
+  print () {
+    let i = this.height;
+    while (i !== 0) {
+      console.log(this.carac.repeat(this.width));
+      i--;
+    }
+  }
+
+  rotate () {
+    this.temp = this.height;
+    this.height = this.width;
+    this.width = this.temp;
+  }
+
+  double () {
+    this.height = this.height * 2;
+    this.width = this.width * 2;
+  }
+};
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (c) {
+      this.carac = 'C';
+      this.print();
+    } else {
+      this.print();
+    }
+  }
+}
+
+module.exports = Rectangle;
+module.exports = Square;
