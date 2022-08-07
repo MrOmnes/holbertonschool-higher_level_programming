30773e87fc460b95ad7774f411bff7953d04fd2a
Omnes <kevin@streamrunners.fr>
2022-08-07 13:45:53 +0000

    forgot to say it's a string

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE states.name\
-    = '{}' ORDER BY states.id".format(sys.argv[4]))
+    = '{:s}' ORDER BY states.id".format(sys.argv[4]))
     result = cursor.fetchall()
 
     for x in result:
