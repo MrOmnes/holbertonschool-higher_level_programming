6d37c141334d5ec6a4528e3eca30714e83ebd880
Omnes <kevin@streamrunners.fr>
2022-08-07 13:47:30 +0000

    try a verification

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -7,11 +7,12 @@ if __name__ == '__main__':
     db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
     cursor = db.cursor()
     cursor.execute("SELECT * FROM states WHERE states.name\
-    = '{:s}' ORDER BY states.id".format(sys.argv[4]))
+    = '{}' ORDER BY states.id".format(sys.argv[4]))
     result = cursor.fetchall()
 
     for x in result:
-        print(x)
+        if (x[1] == sys.argv[4]):
+            print(x)
 
     cursor.close()
     db.close()
