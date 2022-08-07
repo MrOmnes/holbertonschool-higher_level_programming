e97e259e72cf1cd728e56f9a8a755348f40aad59
Omnes <kevin@streamrunners.fr>
2022-08-07 11:48:44 +0000

    add 0

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import MySQLdb;
+import sys;
+db=MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+cursor= db.cursor()
+cursor.execute("SELECT * FROM states ORDER BY id")
+result = cursor.fetchall()
+
+for x in result:
+	print(x)
+
