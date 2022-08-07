4de12c127642f0707b900df469f94666e68161a8
Omnes <kevin@streamrunners.fr>
2022-08-07 12:08:29 +0000

    add 2

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Select * States """
+import MySQLdb
+import sys
+
+if __name__ == '__main__':
+    db = MySQLdb.connect('localhost', sys.argv[1], sys.argv[2], sys.argv[3])
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM states ORDER BY states.id")
+    result = cursor.fetchall()
+
+    for x in result:
+        if x[1] == sys.argv[4]:
+            print(x)
