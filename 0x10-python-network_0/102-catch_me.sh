b2a28841014185153dc6d5042c67ccafde829621
Omnes <kevin@streamrunners.fr>
2022-08-18 19:20:00 +0000

    add this 102

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash                                                                                      
 # Display body if status number eq 200                                                           
-curl -s -L "$1" -X PUT -d 'user_id=98' 0.0.0.0:5000/catch_me
+curl -s -L -X PUT -d 'user_id=98' 0.0.0.0:5000/catch_me -H "Origin: HolbertonSchool"
