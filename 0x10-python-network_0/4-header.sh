85f42aab2718426e73e175cbce9a129f76d58c3b
Omnes <kevin@streamrunners.fr>
2022-08-18 17:07:01 +0000

    add 4

---
+++ 0x10-python-network_0/4-header.sh
@@ -0,0 +1,3 @@
+#!/bin/bash                                                                                      
+# Display body if status number eq 200                                                           
+curl -s -L "$1" -H "X-HolbertonSchool-User-Id: 98"
