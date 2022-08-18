976c2b7871db88ff1c370c540d11d23b19d1d951
Omnes <kevin@streamrunners.fr>
2022-08-18 17:03:01 +0000

    try a fix

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash                                                                                      
 # Display body if status number eq 200                                                           
-curl -i -s -L  "$1" -X OPTIONS | grep -E "Allow:" | cut -d ' ' -f 2,3,4
+curl -i -s -L  "$1" -X OPTIONS | grep -E "Allow:" | cut -d ' ' -f 2-
