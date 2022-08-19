1a29211b24fd64bdc908a18e6aa469e23b1c5a39
Omnes <kevin@streamrunners.fr>
2022-08-19 11:45:48 +0000

    fucking vscode :c

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -4,4 +4,4 @@ import urllib.request
 import sys
 if __name__ == "__main__":
     with urllib.request.urlopen(sys.argv[1]) as response:
-       print(response.getheader('X-Request-Id'))
+        print(response.getheader('X-Request-Id'))
