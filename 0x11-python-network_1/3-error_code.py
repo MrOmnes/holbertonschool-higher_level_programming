2e0f9a16cbdd886ad5dd7f689f14f1eff4f47370
Omnes <kevin@streamrunners.fr>
2022-08-19 13:08:41 +0000

    code improvement

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -6,9 +6,8 @@ import sys
 
 
 if __name__ == "__main__":
-    url = sys.argv[1]
     try:
-        with urllib.request.urlopen(url) as response:
+        with urllib.request.urlopen(sys.argv[1]) as response:
             print(response.read().decode('utf-8'))
     except HTTPError as err:
         print("Error code:", err.code)
