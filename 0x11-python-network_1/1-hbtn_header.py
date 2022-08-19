fe535d2e0964880a10c427053ee7f906847ee1ac
Omnes <kevin@streamrunners.fr>
2022-08-19 10:56:19 +0000

    try a fix

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-# Try a fix with commenting code
-import urllib.request
-import sys
 if __name__ == "__main__":
+    import urllib.request
+    import sys
     with urllib.request.urlopen(sys.argv[1]) as response:
         print(response.getheader('X-Request-Id'))
