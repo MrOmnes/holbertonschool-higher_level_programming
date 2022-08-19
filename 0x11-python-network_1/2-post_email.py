bd2ea8b5ee42b74d6fa433f35401222e581c7615
Omnes <kevin@streamrunners.fr>
2022-08-19 02:39:40 +0000

    add 3 and fix pycode for 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -3,8 +3,8 @@ import urllib.request
 import urllib.parse
 import sys
 url = sys.argv[1]
-params = {'email': sys.argv[2] }
-query_string = urllib.parse.urlencode(params) 
+params = {'email': sys.argv[2]}
+query_string = urllib.parse.urlencode(params)
 data = query_string.encode("ascii")
-with urllib.request.urlopen(url, data) as response:        
+with urllib.request.urlopen(url, data) as response:
     print(response.read().decode('utf-8'))
